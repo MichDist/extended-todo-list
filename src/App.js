11d10df4292c3b8d1c88383c39ca26d500a73d54
Michael Distler <Michael.Distler@web.de>
2020-02-01 21:46:44 +0100

    More State Hooks

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import './App.css';
 //import ReactDOM from 'react-dom'
 import React, { useState } from 'react'
 
+const Display = ({counter}) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+
+const Button = ({eventHandler, label}) => {
+  return (
+    <div>
+      <button onClick={eventHandler}>
+        {label}
+      </button>
+    </div>
+  )
+}
 
 const App = () => {
   const [counter, setCounter] = useState(0);
@@ -13,10 +28,9 @@ const App = () => {
 
   return (
     <div>
-      {counter}
-      <button onClick={handleClick}>
-        plus
-      </button>
+      <Display counter={counter}/>
+      <Button eventHandler={handleClick} label='plus'/>
+      <Button eventHandler={ () => setCounter(0)} label='reset'/>
     </div>
   )
 }
